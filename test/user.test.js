1ffcfd253dcf8772044e042119ebeaca6b50949a
Benson Imoh,ST <benson@stbensonimoh.com>
2020-01-11 10:47:08 +0100

    Add more tests with mock

--- test/user.test.js
+++ test/user.test.js
@@ -1,12 +1,36 @@
-const User = require('../controllers/userCtrl');
+const User = require('../controllers/userCtrl')
+
+beforeEach(() => {
+  return response = {
+    id: 2,
+    firstName: 'Chiamaka',
+    lastName: 'Osumgba'
+  }
+})
+
 // describe('the user', () => {
 // 	test('gets a user from the database', (id) => {
 // 	expect(User(3)).toBe('chiamaka');
 // 	});
 // });
 
-describe('a user', () => {
-	test('getting a user', (id) => {
-		expect(User.getAUserById(2)).toBe('chiamaka osumgba');
-	})
-});
+describe('User', () => {
+  test('getting a user by id', () => {
+    User.getAUserById = jest.fn()
+    const result = { id: 2, firstName: 'Chiamaka', lastName: 'Osumgba' }
+
+    User.getAUserById.mockReturnValue(response)
+
+    expect(User.getAUserById(2)).toEqual(result)
+  })
+
+  test('Must provide id to call a user', () => {
+      User.getAUserById = jest.fn()
+      User.getAUserById(2);
+      expect(User.getAUserById).toHaveBeenCalledWith(2);
+  })
+})
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(User.sum(1, 2)).toBe(3)
+})
